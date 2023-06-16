3b7973427344cf9633b9bc62faae28c332f14183
FABIO FREITAS <fabiojofre@gmail.com>
2023-06-15 22:00:46 -0300

    Redireccionando página home para /sebd

--- src/main/java/com/jofre/sebd/web/controller/HomeColntroller.java
+++ src/main/java/com/jofre/sebd/web/controller/HomeColntroller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeColntroller {
 
-	@GetMapping("/")
+	@GetMapping("/sebd")
 	public String home(){
 		return "/home";
 		
